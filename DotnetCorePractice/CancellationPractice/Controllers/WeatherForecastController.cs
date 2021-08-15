2356c96d65d0438a13c43ea2e8d3db4933c00998
DiorHuang <hehuang@cimpress.com.cn>
2021-08-15 19:53:13 +0800

    add several cases for cancellation token

--- DotnetCorePractice/CancellationPractice/Controllers/WeatherForecastController.cs
+++ DotnetCorePractice/CancellationPractice/Controllers/WeatherForecastController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CancellationPractice.Controllers
@@ -22,6 +23,76 @@ namespace CancellationPractice.Controllers
         {
             _logger = logger;
         }
+
+        [HttpGet("CancelTest")]
+        public async Task<IActionResult> CancelTestAsync(CancellationToken tokenInParam)
+        {
+            //var cancelToken = HttpContext.RequestAborted;
+            var tokenSource = new CancellationTokenSource();
+            var waitTask = Task.Delay(3000, tokenSource.Token);
+            tokenSource.CancelAfter(2500);
+            await waitTask;
+
+            Console.WriteLine("CancelTest Response OK");
+            return Ok("OK");
+        }
+
+        [HttpGet("CancelTestUserCode")]
+        public IActionResult CancelTestUserCode(CancellationToken cancelToken)
+        {
+            //var cancelToken = HttpContext.RequestAborted;
+            for (var i = 0; i < 40; i++)
+            {
+                //cancelToken.ThrowIfCancellationRequested();
+                if (cancelToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("Operation Canceled");
+                    break;
+                }
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("CancelTestUserCode Response OK");
+            return Ok("OK");
+        }
+
+        [HttpGet("CancelTestCooperation")]
+        public async Task<IActionResult> CancelTestCooperationAsync(CancellationToken cancelToken)
+        {
+            cancelToken.Register(() => Console.WriteLine("Callback Canceled."));
+            var task = Task.Run(() =>
+            {
+                for (var i = 0; i < 40; i++)
+                {
+                    cancelToken.ThrowIfCancellationRequested();
+                    //if (cancelToken.IsCancellationRequested)
+                    //{
+                    //    Console.WriteLine("Operation Canceled");
+                    //    throw new OperationCanceledException(cancelToken);
+                    //}
+                    Thread.Sleep(100);
+                }
+            }, cancelToken);
+
+            try
+            {
+                await task;
+            }
+            catch (OperationCanceledException opEx)
+            {
+                Console.WriteLine($"{nameof(OperationCanceledException)} thrown with message: {opEx.Message}");
+            }
+            finally
+            {
+                Console.WriteLine($"Task Status:{task.Status}");
+            }
+
+
+            Console.WriteLine("CancelTestUserCode Response OK");
+            return Ok("OK");
+        }
+
+        // TODO wait handle
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
