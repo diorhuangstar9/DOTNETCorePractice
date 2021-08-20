cd41bf75dfd4696d9059eaf86a430b5faf1a9945
DiorHuang <hehuang@cimpress.com.cn>
2021-08-20 21:53:35 +0800

    UseDefaultServiceProvider

---
+++ DotnetCorePractice/DIPractice/Services/IScopedService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIPractice.Services
+{
+    public interface IScopedService
+    {
+        public string GetScopeId();
+
+    }
+
+    public class ScopedTest1Service : IScopedService
+    {
+        public string GetScopeId()
+        {
+            return $"test1-{Guid.NewGuid().ToString()[^4..]}";
+        }
+    }
+
+    public class ScopedTest2Service : IScopedService
+    {
+        public string GetScopeId()
+        {
+            return $"test2-{Guid.NewGuid().ToString()[^4..]}";
+        }
+    }
+
+    public class ScopedTest3Service : IScopedService
+    {
+        public string GetScopeId()
+        {
+            return $"test3-{Guid.NewGuid().ToString()[^4..]}";
+        }
+    }
+
+
+}
